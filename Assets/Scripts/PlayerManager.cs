7fcc58c76f3d57aea33b9898c3cbaa5e67bc4a1b
Seyidahmetyogurtcu <yogurtcu00@hotmail.com>
2021-09-26 15:54:35 +0300

    ADD GRAVITY
    Attract feature added.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Count_Master_SAY.Control
 {
-
     public class PlayerManager : MonoBehaviour
     {
+        [SerializeField] private List<GameObject> persons = new List<GameObject>();
+        List<Vector3> distanceToCenter = new List<Vector3>();
+        public int fMagnitude = 10;
+        float delay = 0.3f;
+        float timer = 0;
         void Start()
         {
             EventManager.singleton.onPlayerMoveTriggerEnter += OnPlayerMove;
@@ -15,9 +19,28 @@ namespace Count_Master_SAY.Control
         {
             //Todo:Move all player characters horizontaly
             //Debug.Log("Player is moving");
-            transform.Translate(new Vector3(Time.fixedDeltaTime*5, 0,0), Space.Self) ;
+            transform.Translate(new Vector3(Time.fixedDeltaTime * 5, 0, 0), Space.Self);
         }
 
 
+        void Update()
+        {
+            if (Time.timeSinceLevelLoad> timer)
+            {
+                timer = delay + Time.timeSinceLevelLoad;
+                Attract();
+            }       
+        }
+
+        private void Attract()
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                distanceToCenter.Add(this.transform.position - persons[i].transform.position);
+                persons[i].GetComponent<Rigidbody>().AddForce(distanceToCenter[i] * fMagnitude, ForceMode.Force);
+                Debug.Log("calculated");
+            }
+            distanceToCenter.Clear();
+        }
     }
 }
